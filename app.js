aa8191cd0b2d735a24a53039dd865bc90961d5e8
John Ha <haj9@mcmaster.ca>
2021-01-16 03:36:09 -0500

    frontend of homepage done and some categories

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/healthy", function(req, res){
+    res.render("healthy");
+});
+
+app.get("/mild", function(req, res){
+    res.render("mild");
+});
+
+app.get("/severe", function(req, res){
+    res.render("severe");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on localhost:3000");
+});
