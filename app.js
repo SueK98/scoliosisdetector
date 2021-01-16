e96f1a7f8437fd695bdcec3053746034a1f3e4f8
John Ha <haj9@mcmaster.ca>
2021-01-16 13:33:37 -0500

    upload page to be tested

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/severe", function(req, res){
     res.render("severe");
 });
 
+app.get("/testScoliosis", function(req, res){
+    res.render("testScoliosis");
+});
+
 app.listen(3000, function(){
     console.log("Server started on localhost:3000");
 });
